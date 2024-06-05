fe7978ff3691588ae7a6aa0e4d338e79f3b59eaf
BraynerEF <147545983+BraynerEF@users.noreply.github.com>
2024-06-05 13:01:25 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/chat_provider.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ChatProvider(),
+      child: MaterialApp(
+        title: 'Alver',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ChatScreen(),
+      ),
+    );
+  }
+}
